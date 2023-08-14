22f42e9e4b34bfd385d584f1d4bc37f7a9761c67
Alexander_G <le.xander.gl@gmail.com>
2023-08-14 20:23:53 +0300

    JS practise - todos app

---
+++ todo-frontend/todo-app.js
@@ -0,0 +1,136 @@
+function createAppTitle(title) {
+  let appTitle = document.createElement('h2');
+  appTitle.innerHTML = title;
+  return appTitle;
+}
+function createTodoItemForm() {
+  let form = document.createElement('form');
+  let input = document.createElement('input');
+  let buttonWrapper = document.createElement('div');
+  let button = document.createElement('button');
+  form.classList.add('input-group', 'mb-3');
+  input.classList.add('form-control');
+  input.placeholder = 'Введите название нового дела';
+  buttonWrapper.classList.add('input-group-append');
+  button.classList.add('btn', 'btn-primary');
+  button.textContent = 'Добавить дело';
+  button.disabled = true;
+
+  buttonWrapper.append(button);
+  form.append(input);
+  form.append(buttonWrapper);
+
+  input.addEventListener('input', () => {
+    if (input.value.trim() !== '') {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  });
+
+  return {
+    form,
+    input,
+    button,
+  };
+}
+function createTodoList() {
+  // Инициализация функции создания списка дел
+  let list = document.createElement('ul');
+  // Задание переменной списка для списка дел
+  list.classList.add('list-group');
+  // Добавление бутстрап класса для переменной списка дел
+  return list;
+  // Вызов переменной списка дел
+}
+function createTodoItemElement(todoItem, { onDone, onDelete }) {
+  const doneClass = 'list-group-item-success';
+
+  const item = document.createElement('li');
+  item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+  item.textContent = todoItem.name;
+
+  const buttonGroup = document.createElement('div');
+  buttonGroup.classList.add('btn-group', 'btn-group-sm');
+
+  const doneButton = document.createElement('button');
+  doneButton.classList.add('btn', 'btn-success');
+  doneButton.textContent = 'Готово';
+
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('btn', 'btn-danger');
+  deleteButton.textContent = 'Удалить';
+
+  if (todoItem.done) {
+    item.classList.add(doneClass);
+  }
+
+  doneButton.addEventListener('click', () => {
+    onDone({ todoItem, element: item });
+    item.classList.toggle(doneClass, todoItem.done);
+  });
+  deleteButton.addEventListener('click', () => {
+    onDelete({ todoItem, element: item });
+  });
+
+  buttonGroup.append(doneButton);
+  buttonGroup.append(deleteButton);
+  item.append(buttonGroup);
+
+  return item;
+}
+async function createTodoApp(container, title, owner) {
+  const todoAppTitle = createAppTitle(title);
+  const todoItemForm = createTodoItemForm();
+  const todoList = createTodoList();
+  const handlers = {
+    onDone({ todoItem }) {
+      todoItem.done = !todoItem.done;
+      fetch(`http://localhost:3000/api/todos/${todoItem.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ done: todoItem.done }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    },
+    onDelete({ todoItem, element }) {
+      // eslint-disable-next-line no-restricted-globals
+      if (!confirm('Вы уверены?')) {
+        // eslint-disable-next-line no-useless-return
+        return;
+      }
+      element.remove();
+      fetch(`http://localhost:3000/api/todos/${todoItem.id}`, {
+        method: 'DELETE',
+      });
+    },
+  };
+
+  container.append(todoAppTitle);
+  container.append(todoItemForm.form);
+  container.append(todoList);
+
+  const response = await fetch(`http://localhost:3000/api/todos?owner=${owner}`);
+  const todoItemList = await response.json();
+
+  todoItemList.forEach((todoItem) => {
+    let todoItemElement = createTodoItemElement(todoItem, handlers);
+    todoList.append(todoItemElement);
+  });
+
+  todoItemForm.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    if (!todoItemForm.input.value) { return; }
+
+
+
+    let todoItemElement = createTodoItemElement(todoItem, handlers);
+
+    todoList.append(todoItemElement);
+
+    todoItemForm.input.value = '';
+  });
+}
+export { createTodoApp };
